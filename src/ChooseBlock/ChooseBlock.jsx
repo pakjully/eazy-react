f0719fbb9e9943702c3438935eaf89c005f75e4a
Roman Popov <popov.r.k@yandex.ru>
2023-08-16 20:38:34 +0300

    #Noob-21-inproved Refactor ChooseBlock

--- src/ChooseBlock/ChooseBlock.jsx
+++ src/ChooseBlock/ChooseBlock.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Pseudobutton } from '../Buttons/Pseudobutton';
 import './ChooseBlock.scss';
 
 export default function ChooseBlock(props) {
   const {
-    button,
     children,
     heading,
-    link,
-    color,
-    design,
   } = props;
   return (
     <div className="block">
       <p className="block-heading">{heading}</p>
       {children}
-      <Link to={link}>
-        <Pseudobutton
-          text={button}
-          color={color}
-          design={design}
-        />
-      </Link>
     </div>
   );
 }
--- src/Pages/Choose/Choose.jsx
+++ src/Pages/Choose/Choose.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import ChooseBlock from '../ChooseBlock/ChooseBlock';
+import { Link } from 'react-router-dom';
+import { Pseudobutton } from '../../Buttons/Pseudobutton';
+import ChooseBlock from '../../ChooseBlock/ChooseBlock';
+import './Choose.scss';
 
 export function Choose() {
   return (
@@ -8,27 +11,33 @@ export function Choose() {
       <div className="container">
         <ChooseBlock
           heading="Расчет налоговой базы по Interactive Brokers"
-          button="Сделать расчет"
-          link="/orders/new"
-          color="orange"
-          design="choose"
         >
           <p className="block-text">Система автоматически рассчитает налоговую базу, и на выходе вы получите:</p>
           <p className="block-text">- Расчет налоговой базы по IB</p>
           <p className="block-text">- Пояснительную записку</p>
           <p className="block-text">- Файл .dc для самостоятельного дозаполнения декларации</p>
+          <Link to="/orders/new">
+            <Pseudobutton
+              text="Сделать расчет"
+              color="orange"
+              design="choose"
+            />
+          </Link>
         </ChooseBlock>
         <ChooseBlock
           heading="Декларация под ключ"
-          button="Хочу декларацию"
-          link="/declaration-orders/new"
-          color="orange"
-          design="choose"
         >
           <p className="block-text declaration">
             Если Вы не хотите тратить время и разбираться в вычетах, сальдировании и тому подобном,
             то наши специалисты подготовят декларацию и полный пакет документов для Вас.
           </p>
+          <Link to="/declaration-orders/new">
+            <Pseudobutton
+              text="Хочу декларацию"
+              color="orange"
+              design="choose"
+            />
+          </Link>
         </ChooseBlock>
       </div>
     </div>
