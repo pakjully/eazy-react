a30ba6a35800eee69833579c758a055fc13a0af3
Pak Yulia <kristalo4ka@gmail.com>
2023-09-16 20:08:21 +0300

    Noob-22-2 Switch Input creation

---
+++ src/Inputs /SwitchInput/SwitchInput.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './SwitchInput.scss';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export function SwitchInput(props) {
+  const { text, label, name, handleChange } = props;
+  return (
+    <Form.Group as={Row} className="mb-3">
+      <Form.Label column sm={12} md={5}>
+        {text}
+        <p className="label">{label}</p>
+      </Form.Label>
+      <Col sm={12} md={7}>
+        <Form.Check
+          type="switch"
+          id="custom-switch"
+          onChange={handleChange}
+          name={name}
+        />
+      </Col>
+    </Form.Group>
+  );
+}
